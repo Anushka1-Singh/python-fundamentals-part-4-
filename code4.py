33dda1ccae99ebc937e848e1d8ddc5bdcdab95f1
Anushka1-Singh <9651anushka@gmail.com>
2025-12-11 12:31:47 +0530

    added anew file

---
+++ code4.py
@@ -0,0 +1,17 @@
+#ABSTRACTION
+
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+class Lion(Animal):
+    def make_sound(self):
+        print("roar")
+class Cow(Animal):
+    def make_sound(self):
+        print("moo")
+lion = Lion()
+lion.make_sound()
+cow = Cow()
+cow.make_sound() 
